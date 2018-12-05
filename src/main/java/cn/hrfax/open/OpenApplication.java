cd8c1cd01877671337ea56aeea941f12b6b1b18f
pipipapi <h19900410>
2018-12-06 00:29:22 +0800

    Initial commit

---
+++ src/main/java/cn/hrfax/open/OpenApplication.java
@@ -0,0 +1,12 @@
+package cn.hrfax.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApplication.class, args);
+    }
+}
